7824c407b9b56ddebac8343ca5408e128bae9ca1
laevatein2070 <zenzoza007@gail.com>
2022-01-02 16:43:50 +0700

    test commit main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Main from './components/online-simple-circuit';
+import Main from './components/main';
 
 const App = () => {
   return(
